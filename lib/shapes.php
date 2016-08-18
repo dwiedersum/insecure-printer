4e3305342d4eb8ec91936776f2449320edf61653
dwiedersum <d.wiedersum@handelshaus-wagner.de>
2016-08-18 13:44:20 +0200

    Füge um 45° gedrehtes Quadrat zu shapes hinzu.

--- lib/shapes.php
+++ lib/shapes.php
@@ -3,7 +3,7 @@ namespace shapes;
 
 function build_square($value, $i = 0){
     while($i < $value){
-        $square_text .= hash_length($value);
+        $square_text .= hash_length($value) . "\n";
         $i++;
     }
     return $square_text;
@@ -11,19 +11,40 @@ function build_square($value, $i = 0){
 
 function hash_length($value){
     $hash_string = str_repeat("#", $value);
-    if ($value !== 0){
-        return $hash_string . "\n";
-    }
+        return $hash_string;
+}
+
+function whitespace_string($value){
+    $whitespace_string = str_repeat(" ", $value);
+    return $whitespace_string;
 }
 
 function build_arrow($value, $i = 0){
     while ($i < $value){
         $i++;
-        $arrow_text .= hash_length($i);
+        $arrow_text .= hash_length($i) . "\n";
     }
-    while($i !== 0){
-        --$i;
-        $arrow_text .= hash_length($i);
+    while($i - 1 > 0){
+        $i--;
+        $arrow_text .= hash_length($i) . "\n";
     }
     return $arrow_text;
+}
+
+function build_rotated_square($value, $i = 0){
+    while ($i < $value){
+        $i++;
+        $rotated_square_text .= whitespace_string($value - $i) . hash_length($i);
+        $i--;
+        $rotated_square_text .= hash_length ($i) . "\n";
+        $i++;
+    }
+    while ($i - 1 > 0){
+        --$i;
+        $rotated_square_text .= whitespace_string($value - $i) . hash_length($i);
+        $i--;
+        $rotated_square_text .= hash_length ($i) . "\n";
+        $i++;
+    }
+    return $rotated_square_text;
 }
--- test/shapes_test.php
+++ test/shapes_test.php
@@ -13,12 +13,23 @@ class shapes_testcase extends TestCase {
         $this->assertEquals("", shapes\build_square(0));
         $this->assertEquals("#####\n#####\n#####\n#####\n#####\n", shapes\build_square(5));
         $this->assertEquals("##\n##\n", shapes\build_square(2));
+        $this->assertEquals("", shapes\build_square(-1));
     }
 
     public function test_arrow_build(){
         $this->assertEquals("#\n##\n###\n####\n###\n##\n#\n", shapes\build_arrow(4));
         $this->assertEquals("#\n##\n#\n", shapes\build_arrow(2));
         $this->assertEquals("", shapes\build_arrow(0));
+        $this->assertEquals("", shapes\build_arrow(-1));
+    }
+
+    public function test_rotated_square(){
+        $this->assertEquals("   #\n  ###\n #####\n#######\n #####\n  ###\n   #\n",
+                            shapes\build_rotated_square(4));
+        $this->assertEquals(" #\n###\n #\n",
+                            shapes\build_rotated_square(2));
+        $this->assertEquals("", shapes\build_rotated_square(0));
+        $this->assertEquals("", shapes\build_rotated_square(-1));
     }
 
 }
