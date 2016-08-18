9db46a2e90fb68985641473189d09c6cacc6b94c
dwiedersum <d.wiedersum@handelshaus-wagner.de>
2016-08-18 12:27:04 +0200

    Füge zusätzliche Tests hinzu

--- test/shapes_test.php
+++ test/shapes_test.php
@@ -10,6 +10,9 @@ class shapes_testcase extends TestCase {
 
     public function test_square_build(){
         $this->assertEquals("###\n###\n###\n", shapes\build_square(3));
+        $this->assertEquals("", shapes\build_square(0));
+        $this->assertEquals("#####\n#####\n#####\n#####\n#####\n", shapes\build_square(5));
+        $this->assertEquals("##\n##\n", shapes\build_square(2));
     }
 
 }
