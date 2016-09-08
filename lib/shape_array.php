604ba1a50a6a5ba447eac1095da624b5331d27ed
dwiedersum <d.wiedersum@handelshaus-wagner.de>
2016-09-08 15:07:17 +0200

    Erstelle big shape array aus small shape

--- lib/shape_array.php
+++ lib/shape_array.php
@@ -6,3 +6,17 @@ function shape_array($figure){
     $array_of_arrays = array_fill(0, $figure->size, $array);
     return $array_of_arrays;
 }
+
+function big_shape_array($big_figure){
+    $small_figure = shape_array($big_figure->filler);
+    $line_number = $big_figure->repeat * 2;
+    $big_figure_array = array();
+    $line = array();
+    for($i = 0; $i < $big_figure->repeat; $i++){
+        $line = array_merge($line, $small_figure[0]);
+    }
+    for($i = 0; $i < $line_number; $i++){
+        $big_figure_array[$i] = $line;
+    }
+    return $big_figure_array;
+}
--- test/shape_array_test.php
+++ test/shape_array_test.php
@@ -2,10 +2,12 @@
 namespace test\shape_array;
 require("../lib/shape_array.php");
 require_once("../lib/shapes.php");
+require_once("../lib/big_shapes.php");
 
 use PHPUnit\Framework\TestCase;
 //use shapes;
 use shapes\Figure;
+use big_shapes\BigFigure;
 use shape_array as sa;
 
 class big_shapes_testcase extends TestCase {
@@ -22,5 +24,22 @@ class big_shapes_testcase extends TestCase {
                              [1, 1, 1, 1],
                              [1, 1, 1, 1],
                              [1, 1, 1, 1]], sa\shape_array($square));
+    }
+
+    public function test_big_figure_array_with_size_1(){
+        $filler = new Figure("square", 2);
+        $square = new BigFigure("square", 1, $filler);
+        $this->assertEquals([[1, 1],
+                             [1, 1]], sa\big_shape_array($square));
+
+    }
+
+    public function test_big_figure_array_with_size_2(){
+        $filler = new Figure("square", 2);
+        $square = new BigFigure("square", 2, $filler);
+        $this->assertEquals([[1, 1, 1, 1],
+                             [1, 1, 1, 1],
+                             [1, 1, 1, 1],
+                             [1, 1, 1, 1]], sa\big_shape_array($square));
     }
 }
