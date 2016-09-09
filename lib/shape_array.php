a76c15a086e28ce01cb376916dbe7f788333634b
dwiedersum <d.wiedersum@handelshaus-wagner.de>
2016-09-09 10:54:13 +0200

    Vereinfache Funktionsstruktur

--- lib/shape_array.php
+++ lib/shape_array.php
@@ -40,17 +40,22 @@ function triangle_array($figure){
     return $array_of_array;
 }
 
+function create_chunk_with_seperator($element, $repeat, $seperator){
+    $chunk = array();
+    for($i = 0; $i < $repeat; $i++){
+        $chunk = array_merge($chunk, $element);
+        if($i + 1 < $repeat){
+            $chunk[] = $seperator;
+        }
+    }
+    return $chunk;
+}
+
 function build_line_chunk($big_figure){
     $small_figure = shape_array($big_figure->filler);
     $line_chunk = array();
-    foreach($small_figure as $line_array){
-        $line = array();
-        for($i = 0; $i < $big_figure->repeat; $i++){
-            $line = array_merge($line, $line_array);
-            if($i + 1 < $big_figure->repeat){
-                $line[] = 0;
-            }
-        }
+    foreach($small_figure as $figure_line){
+        $line = create_chunk_with_seperator($figure_line, $big_figure->repeat, 0);
         $line_chunk[] = $line;
     }
     return $line_chunk;
@@ -59,13 +64,6 @@ function build_line_chunk($big_figure){
 function big_shape_array($big_figure){
     $line_chunk = build_line_chunk($big_figure);
     $line_number = $big_figure->repeat * $big_figure->filler->size + $big_figure->repeat - 1;
-    $big_figure_array = array();
     $special_line = array_fill(0, $line_number, 0);
-    for($i = 0; $i < $big_figure->repeat; $i++){
-        $big_figure_array = array_merge($big_figure_array, $line_chunk);
-        if($i +1 < $big_figure->repeat){
-            $big_figure_array[] = $special_line;
-        }
-    }
-    return $big_figure_array;
+    return create_chunk_with_seperator($line_chunk, $big_figure->repeat, $special_line);
 }
